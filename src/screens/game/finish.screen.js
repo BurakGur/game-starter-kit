54009c575434100a08f61390e52de522880b1f22
burakgur <gurbrk@gmail.com>
2022-10-30 21:53:08 +0300

    continue ad integration

--- src/screens/game/finish.screen.js
+++ src/screens/game/finish.screen.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {GameLayout} from '@layouts';
-import {Text, TouchableOpacity} from 'react-native';
+import {ActivityIndicator, StatusBar, Text, TouchableOpacity} from 'react-native';
 import {useRecoilValue} from 'recoil';
 import {userState} from '@/store/atoms';
 import api from '@services/api';
@@ -9,19 +9,55 @@ import {themeState} from '@/store/selectors';
 import {AppButton, AppText, ArticleCard} from '@/components';
 import {redis} from '@utils/redis';
 import {updateUserScore} from '@utils/user';
+import {RewardedAd, RewardedAdEventType, TestIds} from 'react-native-google-mobile-ads';
+
+const adUnitId = __DEV__ ? TestIds.REWARDED : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
+
+const rewarded = RewardedAd.createForAdRequest(adUnitId, {
+  requestNonPersonalizedAdsOnly: true,
+  keywords: ['fashion', 'clothing'],
+});
 
 const FinishGameScreen = ({navigation, route}) => {
   const user = useRecoilValue(userState);
   const finishScore = route.params.score;
   const isHighScore = finishScore > user.score;
   const highScore = isHighScore ? finishScore : user.score;
+  const [loaded, setLoaded] = useState(false);
+  StatusBar.setHidden(true);
 
   useEffect(() => {
     if (isHighScore) {
       redis.set(user.username, finishScore);
       updateUserScore(user, finishScore);
     }
+
+    const unsubscribeLoaded = rewarded.addAdEventListener(RewardedAdEventType.LOADED, () => {
+      setLoaded(true);
+    });
+    const unsubscribeEarned = rewarded.addAdEventListener(RewardedAdEventType.EARNED_REWARD, reward => {
+      navigation.navigate('PlayGame');
+    });
+
+    // Start loading the rewarded ad straight away
+    rewarded.load();
+
+    // Unsubscribe from events on unmount
+    return () => {
+      unsubscribeLoaded();
+      unsubscribeEarned();
+    };
   }, []);
+
+  // No advert ready to show yet
+  if (!loaded) {
+    rewarded.load();
+    return (
+      <GameLayout>
+        <ActivityIndicator />
+      </GameLayout>
+    );
+  }
 
   return (
     <GameLayout>
@@ -35,7 +71,7 @@ const FinishGameScreen = ({navigation, route}) => {
             routes: [{name: 'PlayGame'}],
           })
         }></AppButton>
-      <AppButton title="Devam Et" onPress={() => navigation.navigate('PlayGame')}></AppButton>
+      <AppButton title="Devam Et" onPress={() => rewarded.show()}></AppButton>
     </GameLayout>
   );
 };
--- src/screens/game/start.screen.js
+++ src/screens/game/start.screen.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import {GameLayout} from '@layouts';
 import {Text, TouchableOpacity} from 'react-native';
 import {useRecoilValue} from 'recoil';
@@ -7,9 +7,37 @@ import api from '@services/api';
 import styled from 'styled-components/native';
 import {themeState} from '@/store/selectors';
 import {AppButton, AppText, ArticleCard} from '@/components';
+import {AppState, StyleSheet, View} from 'react-native';
+import {AppOpenAd, TestIds, AdEventType} from 'react-native-google-mobile-ads';
+
+const adUnitId = __DEV__ ? TestIds.APP_OPEN : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
+
+const appOpenAd = AppOpenAd.createForAdRequest(adUnitId, {
+  requestNonPersonalizedAdsOnly: true,
+  keywords: ['fashion', 'clothing'],
+});
 
 const StartGameScreen = ({navigation}) => {
   const user = useRecoilValue(userState);
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
+
+  useEffect(() => {
+    appOpenAd.load();
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      if (appState.current.match(/inactive|background/) && nextAppState === 'active') {
+        appOpenAd.show();
+      }
+
+      appState.current = nextAppState;
+      setAppStateVisible(appState.current);
+      console.log('AppState', appState.current);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
 
   return (
     <GameLayout>
